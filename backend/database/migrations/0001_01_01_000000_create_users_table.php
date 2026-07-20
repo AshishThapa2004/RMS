1a520a0dfe597a4f949570ca3662caca7d2da855
chetri-anjan <chetrrianjan@gmail.com>
2026-07-20 22:18:56 +0545

    Save my local changes before pulling

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,14 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('user_id');
-            $table->string('name', 100);
+            $table->id();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone', 15)->unique();
-            $table->string('password', 100);
-            $table->enum('role', ['admin','worker','kitchen']);
-            $table->enum('status', ['active', 'inactive']);
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
